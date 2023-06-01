4c7b61f89847932aa71c69534faf84e908906a2e
Hanzhen Yi <hzyi@google.com>
2023-05-31 22:36:54 -0700

    working example

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yihanzhen/gcb-go-pull
+
+go 1.18
+
+require github.com/yihanzhen/gcb-go-push v0.4.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yihanzhen/gcb-go-push/consts"
+)
+
+func main() {
+	fmt.Print(consts.HelloWorld)
+}
